59c9cda09db6edbd7ff9563a120c3648ce4d715b
AleCigolini <cigolini.ale@gmail.com>
2025-07-01 20:34:37 -0300

    Adicionado testes unitarios para RequestPedidoModelMapper

---
+++ src/test/java/br/com/fiap/techchallengeapipedidoproduto/pedido/application/mapper/model/RequestPedidoModelMapperTest.java
@@ -0,0 +1,91 @@
+package br.com.fiap.techchallengeapipedidoproduto.pedido.application.mapper.model;
+
+import br.com.fiap.techchallengeapipedidoproduto.core.config.exception.exceptions.ValidacaoEntidadeException;
+import br.com.fiap.techchallengeapipedidoproduto.pedido.common.domain.dto.request.PedidoRequestDto;
+import br.com.fiap.techchallengeapipedidoproduto.pedido.common.domain.dto.request.PedidoStatusRequestDto;
+import br.com.fiap.techchallengeapipedidoproduto.pedido.domain.Pedido;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.MappingException;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.ErrorMessage;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+public class RequestPedidoModelMapperTest {
+
+    private ModelMapper modelMapper;
+    private RequestPedidoModelMapper mapper;
+
+    @BeforeEach
+    public void setUp() {
+        modelMapper = Mockito.mock(ModelMapper.class);
+        mapper = new RequestPedidoModelMapper(modelMapper);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Mockito.reset(modelMapper);
+    }
+
+    @Test
+    public void deveConverterPedidoRequestDtoParaPedido() {
+        // given
+        PedidoRequestDto pedidoRequestDto = new PedidoRequestDto();
+        when(modelMapper.map(pedidoRequestDto, Pedido.class)).thenReturn(new Pedido());
+
+        // when
+        Pedido result = mapper.pedidoRequestDtoParaPedido(pedidoRequestDto);
+
+        // then
+        assertNotNull(result);
+    }
+
+    @Test
+    public void deveLancarValidacaoEntidadeExceptionAoConverterPedidoRequestDtoParaPedido() {
+        // given
+        PedidoRequestDto pedidoRequestDto = new PedidoRequestDto();
+        MappingException mappingException = new MappingException(List.of(new ErrorMessage("Erro de validação", new ValidacaoEntidadeException("Erro de validação"))));
+        when(modelMapper.map(pedidoRequestDto, Pedido.class)).thenThrow(mappingException);
+
+        // when
+        ValidacaoEntidadeException exception = assertThrows(ValidacaoEntidadeException.class, () -> mapper.pedidoRequestDtoParaPedido(pedidoRequestDto));
+
+        // then
+        assertEquals("Erro de validação", exception.getMessage());
+    }
+
+    @Test
+    public void deveLancarMappingExceptionAoConverterPedidoRequestDtoParaPedido() {
+        // given
+        PedidoRequestDto pedidoRequestDto = new PedidoRequestDto();
+        MappingException mappingException = new MappingException(List.of(new ErrorMessage("Erro de validação")));
+
+        when(modelMapper.map(pedidoRequestDto, Pedido.class)).thenThrow(mappingException);
+
+        // when
+        // then
+        assertThrows(MappingException.class, () -> mapper.pedidoRequestDtoParaPedido(pedidoRequestDto));
+    }
+
+    @Test
+    public void deveConverterPedidoStatusRequestDtoParaPedido() {
+        // given
+        PedidoStatusRequestDto pedidoStatusRequestDto = new PedidoStatusRequestDto();
+        pedidoStatusRequestDto.setStatus("PRONTO");
+        String id = "123";
+
+        // when
+        Pedido result = mapper.pedidoStatusRequestDtoParaPedido(pedidoStatusRequestDto, id);
+
+        // then
+        assertNotNull(result);
+        assertEquals(id, result.getId());
+        assertEquals("PRONTO", result.getStatus());
+    }
+}
