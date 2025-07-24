0652ff41644ecf5f92da99dafbb60502b1237e52
AleCigolini <cigolini.ale@gmail.com>
2025-07-23 21:34:47 -0300

    Removida importação não utilizada no teste de ConsultarPedidoUseCaseImpl

--- src/test/java/br/com/fiap/techchallengeapipedidoproduto/pedido/application/usecase/impl/ConsultarPedidoUseCaseImplTest.java
+++ src/test/java/br/com/fiap/techchallengeapipedidoproduto/pedido/application/usecase/impl/ConsultarPedidoUseCaseImplTest.java
@@ -15,7 +15,6 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 public class ConsultarPedidoUseCaseImplTest {
