682aca59aea6b23e54c86456f00e1eeab6620e34
AleCigolini <cigolini.ale@gmail.com>
2025-07-29 19:29:45 -0300

    Alterada a configuração do caminho do contexto do servidor para /pedido-produto e atualizado o redirecionamento do Swagger para refletir a nova URL.

--- src/main/java/br/com/fiap/techchallengeapipedidoproduto/core/config/config/SwaggerRedirectController.java
+++ src/main/java/br/com/fiap/techchallengeapipedidoproduto/core/config/config/SwaggerRedirectController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SwaggerRedirectController {
 
-    @GetMapping("/")
+    @GetMapping("/swagger")
     public String redirectToSwagger() {
-        return "redirect:/swagger-ui/index.html";
+        return "redirect:/pedido-produto/swagger-ui/index.html";
     }
 }
