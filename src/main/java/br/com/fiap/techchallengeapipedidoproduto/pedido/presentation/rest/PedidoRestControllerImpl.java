fb4171df9709ca1a7fd5e1e3a6cb6051c465cfd8
AleCigolini <cigolini.ale@gmail.com>
2025-07-29 21:15:34 -0300

    Atualizada a configuração do caminho do contexto do servidor para /pedidos-produtos e ajustado o redirecionamento do Swagger para a nova URL.

--- src/main/java/br/com/fiap/techchallengeapipedidoproduto/pedido/presentation/rest/PedidoRestControllerImpl.java
+++ src/main/java/br/com/fiap/techchallengeapipedidoproduto/pedido/presentation/rest/PedidoRestControllerImpl.java
@@ -16,7 +16,7 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/pedidos")
+@RequestMapping("pedidos-produtos/pedidos")
 @RequiredArgsConstructor
 public class PedidoRestControllerImpl implements PedidoRestController {
 
